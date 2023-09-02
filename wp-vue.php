7870bf755d966f8dda16fa5cd0ad58417022e16d
bisnukundu <bisnukundu@gmail.com>
2023-09-03 03:07:07 +0600

    change the plugin name

--- wp-vue.php
+++ wp-vue.php
@@ -1,74 +1,68 @@
 <?php
 
 /**
- * Plugin Name: WP Vue
- * Description: WP Vue
- * Author URI:  https://hasin.me
- * Plugin URI:  https://hasin.me
+ * Plugin Name: Custom Form Bisnu
+ * Description: It's a custom form created by Bisnu Kundu as Carlos mate requirements.
+ * Author URI:  http://github.com/bisnukundu
+ * Plugin URI:  http://github.com/bisnukundu
  * Version:     1.0.0
- * Author:      Hasin
+ * Author:      Bisnu Kundu
  * Text Domain: wp-vue
  * Domain Path: /i18n
  */
 
 include_once ABSPATH . 'wp-admin/includes/plugin.php';
 
-class WPVue
-{
-    public function __construct()
-    {
+class WPVue {
+    public function __construct() {
         // add_action('init',[$this,'initialize']);
-        add_action('wp_enqueue_scripts', [$this, 'loadAssets']);
-        add_filter('script_loader_tag', [$this, 'loadScriptAsModule'], 10, 3);
+        add_action( 'wp_enqueue_scripts', [$this, 'loadAssets'] );
+        add_filter( 'script_loader_tag', [$this, 'loadScriptAsModule'], 10, 3 );
     }
 
-    public function loadScriptAsModule($tag, $handle, $src)
-    {
-        if ('wp-vue-core' !== $handle) {
+    public function loadScriptAsModule( $tag, $handle, $src ) {
+        if ( 'wp-vue-core' !== $handle ) {
             return $tag;
         }
-        $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
+        $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
         return $tag;
     }
 
-    public function loadAssets()
-    {
-        $pluginUrl = plugin_dir_url(__FILE__);
+    public function loadAssets() {
+        $pluginUrl = plugin_dir_url( __FILE__ );
 
-        wp_enqueue_script('wp-vue-core', '//localhost:5173/src/main.js', [], time(), true);
-        // wp_enqueue_script('wp-vue-core', plugin_dir_url(__FILE__) . '/dist/assets/index-a6bb0e59.js', [], time(), true);
-        // wp_enqueue_style('wp-vue-core', plugin_dir_url(__FILE__) . '/dist/assets/index-66c967a1.css', [], time());
-        wp_localize_script('wp-vue-core', 'wpvue', [
+        // wp_enqueue_script('wp-vue-core', '//localhost:5173/src/main.js', [], time(), true);
+        wp_enqueue_script( 'wp-vue-core', plugin_dir_url( __FILE__ ) . '/dist/assets/index-8f49c5a2.js', [], time(), true );
+        wp_enqueue_style( 'wp-vue-core', plugin_dir_url( __FILE__ ) . '/dist/assets/index-5b250a0c.css', [], time() );
+        wp_localize_script( 'wp-vue-core', 'wpvue', [
             'url' => $pluginUrl,
-        ]);
+        ] );
     }
 
 }
 
 new WPVue();
 
-function my_vue_shortcode()
-{
+function my_vue_shortcode() {
 
     $html = '<div id="app"></div>';
 
     echo $html;
 }
 
-add_shortcode('custom_form_bisnu', 'my_vue_shortcode');
+add_shortcode( 'custom_form_bisnu', 'my_vue_shortcode' );
 
-add_action('wp_ajax_nopriv_load_more_product', 'submit_multi_step_form');
-add_action('wp_ajax_load_more_product', 'submit_multi_step_form');
+add_action( 'wp_ajax_nopriv_load_more_product', 'submit_multi_step_form' );
+add_action( 'wp_ajax_load_more_product', 'submit_multi_step_form' );
 
-function submit_multi_step_form()
-{
+function submit_multi_step_form() {
     $response = array(
         'message' => $_POST,
     );
 
     // return wp_send_json($response);
 
-    $admin_email = get_option('admin_email');
+    $admin_email = get_option( 'admin_email' );
     $subject = 'Hello, Admin!';
     $message = '<html>
 <head></head>
@@ -81,7 +75,7 @@ function submit_multi_step_form()
         </tr>';
 
     $keys = [
-		'location',
+        'location',
         'country_code',
         'phone',
         'house_rate',
@@ -98,14 +92,14 @@ function submit_multi_step_form()
         'name',
         'radio_one',
         'saving_amout',
-		'saving_parcentage',
-        
+        'saving_parcentage',
+
     ];
 
-    foreach ($keys as $key) {
+    foreach ( $keys as $key ) {
         $message .= '<tr>';
         $message .= '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">' . $key . '</td>';
-        $message .= '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">' . (isset($_POST[$key]) ? $_POST[$key] : '') . '</td>';
+        $message .= '<td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">' . ( isset( $_POST[$key] ) ? $_POST[$key] : '' ) . '</td>';
         $message .= '</tr>';
     }
 
@@ -113,15 +107,15 @@ function submit_multi_step_form()
 </body>
 </html>';
 
-    $headers = array('Content-Type: text/html; charset=UTF-8');
+    $headers = array( 'Content-Type: text/html; charset=UTF-8' );
 
-    $sent = wp_mail($admin_email, $subject, $message, $headers);
+    $sent = wp_mail( $admin_email, $subject, $message, $headers );
 
-    if ($sent) {
+    if ( $sent ) {
 
-        return wp_send_json_success($response, 200);
+        return wp_send_json_success( $response, 200 );
     } else {
-        return wp_send_json_success($sent, 404);
+        return wp_send_json_success( $sent, 404 );
     }
 
 };
